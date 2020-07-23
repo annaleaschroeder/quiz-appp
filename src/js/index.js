4dbd7d8786c3521cfaf77a10b33757fa4dfaaf0a
annaleaschroeder <67912360+annaleaschroeder@users.noreply.github.com>
2020-07-23 19:05:57 +0200

    Fix form

--- src/js/index.js
+++ src/js/index.js
@@ -1,59 +1,45 @@
-/*const buttonAnswer = document.querySelector('.card__button--answer')
-const answer = document.querySelector('.answer-text')
+// constanten, die zum Inhalt (=body) der vier verschiedenen Screens führen
+const pageHome = get('.page-questions')
+const pageBookmarks = get('.page-bookmarks')
+const pageCreate = get('.page-create')
+const pageProfile = get('.page-profile')
 
-buttonAnswer.addEventListener('click', () => {
-  answer.classList.toggle('display-none')
+//constanten, die die Eingabe-Buttons in der Navbar repräsentieren
+const homeNav = get('.footer__icon--home')
+const bookmarksNav = get('.footer__icon--bookmarks')
+const createNav = get('.footer__icon--cards')
+const profileNav = get('.footer__icon--profile')
+
+//Funktion: Bei einem Click auf eins der Icons in der Nav-Bar soll der User zum entsprechenden Screen weiter geleitet werden
+homeNav.addEventListener('click', () => {
+  pageHome.classlist.remove('display-none')
+  pageBookmarks.classList.add('display-none')
+  pageCreate.classList.add('display-none')
+  pageProfile.classList.add('display-none')
 })
 
-let currentPage = document.querySelector('.page-questions')
-const buttonBookmarks = document.querySelector('.footer__icon--bookmarks')
-const pageBookmarks = document.querySelector('.page-bookmarks')
-const buttonCreate = document.querySelector('.footer__icon--cards')
-const pageCreate = document.querySelector('.page-create')
-const buttonProfile = document.querySelector('.footer__icon--profile')
-const pargeProfile = document.querySelector('.page-profile')
-
-
-buttonBookmarks.addEventListener('click', () => {
+bookmarksNav.addEventListener('click', () => {
+  pageHome.classList.add('display-none')
   pageBookmarks.classList.remove('display-none')
-  currentPage.classList.add('display-none')
-  currentPage = document.querySelector('page-bookmarks')
+  pageCreate.classList.add('display-none')
+  pageProfile.classList.add('display-none')
 })
 
-buttonCreate.addEventListener('click', () => {
+createNav.addEventListener('click', () => {
+  pageHome.classList.add('display-none')
+  pageBookmarks.classList.add('display-none')
   pageCreate.classList.remove('display-none')
-  currentPage.classList.add('display-none')
-  currentPage = document.querySelector('page-create')
+  pageProfile.classList.add('display-none')
 })
-*/
 
-const buttonQuestions = document.querySelector('.footer__icon--profile')
-const buttonBookmarks = document.querySelector('.footer__icon--bookmarks')
-const buttonCreate = document.querySelector('.footer__icon--cards')
-const buttonProfile = document.querySelector('.footer__icon--home')
-const pageQuestions = document.querySelector('.page-questions')
-const pageBookmarks = document.querySelector('.page-bookmarks')
-const pageCreate = document.querySelector('.page-create')
-const pageProfile = document.querySelector('.page-profile')
-let currentPage = pageQuestions
-
-buttonQuestions.addEventListener('click', () => {
-  pageQuestions.classList.remove('display-none')
-  currentPage.classList.add('display-none')
-  currentPage = pageQuestions
-})
-buttonBookmarks.addEventListener('click', () => {
-  pageBookmarks.classList.remove('display-none')
-  currentPage.classList.add('display-none')
-  currentPage = pageBookmarks
-})
-buttonCreate.addEventListener('click', () => {
-  pageCreate.classList.remove('display-none')
-  currentPage.classList.add('display-none')
-  currentPage = pageCreate
-})
-buttonProfile.addEventListener('click', () => {
+profileNav.addEventListener('click', () => {
+  pageHome.classList.add('display-none')
+  pageBookmarks.classList.add('display-none')
+  pageCreate.classList.add('display-none')
   pageProfile.classList.remove('display-none')
-  currentPage.classList.add('display-none')
-  currentPage = pageProfile
 })
+
+//function delcaration: get, um querySelector in der constant declaration raus kürzen zu
+function get(selector) {
+  return document.querySelector(selector)
+}
